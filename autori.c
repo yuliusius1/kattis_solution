fe3e648dc7b9bc36d154c6b4c069ca7d2cef6088
yuliusius1 <46050557+yuliusius1@users.noreply.github.com>
2021-07-26 15:51:33 +0700

    Solved Autori with C

---
+++ autori.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char buffer[101];
+    scanf("%s", buffer);
+    size_t len = strlen(buffer);
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        if (buffer[i] < 91 && buffer[i] > 64)
+        {
+            printf("%c", buffer[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
